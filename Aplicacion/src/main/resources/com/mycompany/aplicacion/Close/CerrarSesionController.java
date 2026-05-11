ab9a5c55524f82e7e6b5bca5b6f77e3bb62eaa74
victoria <payapovedav@gmail.com>
2026-05-11 12:13:54 +0200

    He acabado la ventana de registro y cerrar sesión

--- Aplicacion/src/main/resources/com/mycompany/aplicacion/Close/CerrarSesionController.java
+++ Aplicacion/src/main/resources/com/mycompany/aplicacion/Close/CerrarSesionController.java
@@ -2,23 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mycompany.aplicacion.register;
+package com.mycompany.aplicacion.Close;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.StackPane;
 
 /**
  * FXML Controller class
  *
- * @author Tomas
+ * @author payap
  */
-public class RegistroController implements Initializable {
-
-    @FXML
-    private StackPane StackPane_Registro;
+public class CerrarSesionController implements Initializable {
 
     /**
      * Initializes the controller class.
